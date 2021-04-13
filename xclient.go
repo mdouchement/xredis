2a328a91298619dddf47e9742965f73abdc438da
mdouchement <marc.douchement@gmail.com>
2021-04-13 17:45:23 +0200

    Add generic Client interface

---
+++ xclient.go
@@ -0,0 +1,49 @@
+package xredis
+
+import (
+	"context"
+	"strings"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type (
+	// A XClient wraps default Redis client and adds more features.
+	XClient interface {
+		Client
+		Run(ctx context.Context, script Script, keys []string, args ...interface{}) (Value, error)
+		RunOnce(ctx context.Context, script Script, keys []string, args ...interface{}) (Value, error)
+	}
+
+	client struct {
+		*redis.Client
+	}
+)
+
+// New returns a new Client.
+func New(r *redis.Client) XClient {
+	return &client{
+		Client: r,
+	}
+}
+
+// Runs excutes the given static script.
+// It loads the script in the Redis script cache.
+func (c *client) Run(ctx context.Context, script Script, keys []string, args ...interface{}) (Value, error) {
+	r := c.Client.EvalSha(ctx, script.Hash(), keys, args...)
+	if err := r.Err(); err != nil && strings.HasPrefix(err.Error(), "NOSCRIPT ") {
+		c.Client.ScriptLoad(ctx, script.Source()) // ignoring error, relying on uncached EVAL
+
+		r = c.Client.Eval(ctx, script.Source(), keys, args...) // Fallback
+	}
+
+	v, err := r.Result()
+	return &value{value: v}, err
+}
+
+// Runs excutes the given dynamic script.
+// It does not load the script in the Redis script cache.
+func (c *client) RunOnce(ctx context.Context, script Script, keys []string, args ...interface{}) (Value, error) {
+	v, err := c.Client.Eval(ctx, script.Source(), keys, args...).Result()
+	return &value{value: v}, err
+}
